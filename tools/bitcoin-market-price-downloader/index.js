7c709de72c74ecdff59206d7ec359d0302d13938
brunocalou <brunocaloualves@gmail.com>
2016-06-26 12:39:37 -0300

    Changed bitcoin market price api to be compatible with existing code

--- tools/bitcoin-market-price-downloader/index.js
+++ tools/bitcoin-market-price-downloader/index.js
@@ -1,18 +1,35 @@
 #!/usr/bin/nodejs
 
 const chalk = require('chalk');
-const https = require('https');
+const http = require('http');
 const config = require('./Util/config');
 const fs = require('fs');
 const path = require('path');
 const mkdirp = require('mkdirp');
 
-const url = 'https://blockchain.info/pt/charts/market-price?showDataPoints=false&timespan=all&show_header=true&daysAverageString=1&scale=0&format=csv&address=';
+function today() {
+	function formatNumber(number) {
+		if (number < 10) {
+			number =  '0' + number;
+		} else {
+			number = '' + number;
+		}
+		return number;
+	}
+	var dateObj = new Date();
+	var month = formatNumber(dateObj.getUTCMonth() + 1); //months from 1-12
+	var day = formatNumber(dateObj.getUTCDate());
+	var year = dateObj.getUTCFullYear();
+
+	return year + "-" + month + "-" + day;
+}
+
+const url = 'http://api.coindesk.com/v1/bpi/historical/close.csv?start=2010-07-18&end=' + today() + '&index=USD';
 const output_file_path = path.join(config.outputFolder, config.name);
 
 mkdirp.sync(config.outputFolder);
 
-var request = https.get(url, function(response) {
+var request = http.get(url, function(response) {
     var final_data = "";
 
     response.on('data', function(data) {
